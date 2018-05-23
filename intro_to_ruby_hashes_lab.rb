677dfb9dd48ffdd1c474743a6ab5cfb7f4955797
Adrian Davis <chuck.d.kc@gmail.com>
2018-05-23 02:59:12 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -5,16 +5,13 @@ end
 
 def actor
   actor={:name=>"Dwayne The Rock Johnson"}
-
 end
 
 def monopoly
 	monopoly = {
 	  :railroads=>{
-	  
 	  }
 	}
-
 end
 
 def monopoly_with_second_tier
@@ -42,7 +39,6 @@ def monopoly_with_third_tier
 	      :two_pieces_owned=>50, 
 	      :three_pieces_owned=>100, 
 	      :four_pieces_owned=>200}
-	    
 	  }
 	  }
 end
@@ -62,10 +58,8 @@ def monopoly_with_fourth_tier
 	      :two_pieces_owned=>50, 
 	      :three_pieces_owned=>100, 
 	      :four_pieces_owned=>200}
-	    
 	  }
 	  }
-	
 end
 
 
